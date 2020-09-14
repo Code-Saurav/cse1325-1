5ef48ec2c2673050a6e3def007b7155d96b414c1
hprabesh <humagain.prabesh@outlook.com>
2020-09-14 11:42:38 -0500

    Added guess number and attempt

--- P03/full_credit/main.cpp
+++ P03/full_credit/main.cpp
@@ -6,16 +6,15 @@ int main(){
     std::cout<<std::endl<<"Number of tiles? ";
     std::cin>>num_tiles;
     Board matching_words{num_tiles};
-    
+    int guess_num=0;
     std::cout<<std::endl<<matching_words.to_string();
     while (matching_words.solved()==false){
-        std::cout<<std::endl<<"Guess 3, select 2 unmatched tiles: ";
+        guess_num++;
+        std::cout<<std::endl<<"Guess "<< guess_num<<", select 2 unmatched tiles: ";
         std::cin>>tile1>>tile2;
         std::cout<<std::endl<<matching_words.attempt(tile1,tile2);
         std::cout<<std::endl<<matching_words.to_string();
-
     }
-    
-    
+    std::cout<<std::endl<<"Congratulations - you won in "<<guess_num<< " number of guesses"<<std::endl;  
     return 0;
 }
