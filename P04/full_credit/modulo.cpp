58446eb8bb4bdf8a1b4e48ea3168df744d2089d1
hprabesh <humagain.prabesh@outlook.com>
2020-09-20 14:31:57 -0500

    Fixed second test

--- P04/full_credit/modulo.cpp
+++ P04/full_credit/modulo.cpp
@@ -1,6 +1,7 @@
 #include "modulo.h"
 
-Modulo::Modulo(int modulo, int value, int offset):_value{value},_modulo{modulo},_offset{offset}{};
+Modulo::Modulo(int modulo, int value, int offset):_value{value},_modulo{modulo},_offset{offset}{
+};
 
 int Modulo::value(){
     
@@ -11,46 +12,48 @@ void Modulo::set_nmsd(Modulo *nmsd){
 }
 
 Modulo& Modulo::operator+=(int rhs){
-    _value=_value+rhs;
-    if (_value>=_modulo){
-        _value=(_value)%_modulo;
-        _nmsd->_value=(_nmsd->_value)%(_nmsd->_modulo);
-        _nmsd->_value++;
+    this->_value=this->_value+rhs;
+    if (this->_value>=this->_modulo){
+        this->_value=(this->_value)%this->_modulo;
+        if (_nmsd!=NULL){
+            _nmsd->_value=(_nmsd->_value)%(_nmsd->_modulo);
+            _nmsd->operator+=(1);
+        }
     }
     return *this;
 }
 Modulo Modulo::operator+(int rhs){
-    _value=_value+rhs;
+    Modulo modulo {*this};
+    modulo._value=modulo._value+rhs;
+    if (modulo._value>=modulo._modulo){
+        modulo._value=(modulo._value)%modulo._modulo;
+        if (_nmsd!=NULL){
+            _nmsd->_value=(_nmsd->_value)%(_nmsd->_modulo);
+            _nmsd->_value++;
+        }
+    }
+    return modulo;
+}
+Modulo& Modulo::operator++(){ //preincrement
+    _value=(_value+1);
+
     if (_value>=_modulo){
         _value=(_value)%_modulo;
-        _nmsd->_value=(_nmsd->_value)%(_nmsd->_modulo);
-        _nmsd->_value++;
+        if (_nmsd!=NULL){
+            _nmsd->_value=(_nmsd->_value)%(_nmsd->_modulo);
+            _nmsd->_value++;
+        }
     }
     return *this;
 }
-Modulo Modulo::operator++(int ignored){
-    _value=_value+1;
-    if (_value>=_modulo){
-        _value=(_value)%_modulo;
-        _nmsd->_value=(_nmsd->_value)%(_nmsd->_modulo);
-        _nmsd->_value++;
-    }
-    return *this;
-}
-Modulo& Modulo::operator++(){
-    Modulo *temp{this};
-    _value=_value+1;
-    if (_value>=_modulo){
-        _value=(_value)%(_modulo);
-        _nmsd->_value++;
-    }
-    return *temp;
+Modulo Modulo::operator++(int ignored){ //post increment
+    Modulo modulo{*this};
+    ++*this;
+    return modulo;
 }
 
-std::ostream& operator<<(std::ostream& ost, const Modulo& m){
-    ost<< m._value;
-    return ost;
-}
+
+
 
 int Modulo::compare(const int rhs){
     int i=0; 
@@ -60,6 +63,10 @@ int Modulo::compare(const int rhs){
     //to prevent collision: chose odd number because their sum will be even
     return i; 
     
+}
+std::ostream& operator<<(std::ostream& ost, const Modulo& m){
+    ost<< m._value;
+    return ost;
 }
 std::istream& operator>>(std::istream& ost, Modulo& m){
     
