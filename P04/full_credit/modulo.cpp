36348faaa61dda7e371698b7173fb4c19a86e171
hprabesh <humagain.prabesh@outlook.com>
2020-09-21 11:20:07 -0500

    Fixed the issue with _offset and seg fault

--- P04/full_credit/modulo.cpp
+++ P04/full_credit/modulo.cpp
@@ -1,6 +1,7 @@
 #include "modulo.h"
 
 Modulo::Modulo(int modulo, int value, int offset):_value{value},_modulo{modulo},_offset{offset}{
+    _value=(_value-_offset)%(_modulo);
 };
 
 int Modulo::value(){
@@ -12,7 +13,7 @@ void Modulo::set_nmsd(Modulo *nmsd){
 
 Modulo& Modulo::operator+=(int rhs){
     this->_value=this->_value+rhs;
-    if (this->_value>=(this->_modulo+this->_offset)){ // compare with offset value and modulo rather than updating the streaming out
+    if (this->_value>=(this->_modulo)){ // compare with offset value and modulo rather than updating the streaming out
         this->_value=(this->_value)%(this->_modulo);
         if (this->_nmsd!=NULL){
             this->_nmsd->_value=(this->_nmsd->_value)%(this->_nmsd->_modulo); // just in case the seg fault occurs
@@ -21,6 +22,7 @@ Modulo& Modulo::operator+=(int rhs){
     }
     return *this;
 }
+
 Modulo Modulo::operator+(int rhs){
     Modulo modulo {*this};
     modulo._value=modulo._value+rhs;
@@ -35,7 +37,6 @@ Modulo Modulo::operator+(int rhs){
 }
 Modulo& Modulo::operator++(){ //preincrement
     this->_value=(this->_value+1);
-
     if (this->_value>=(this->_modulo+this->_offset)){
         this->_value=(this->_value)%this->_modulo;
         if (this->_nmsd!=NULL){
@@ -51,27 +52,23 @@ Modulo Modulo::operator++(int ignored){ //post increment
     return modulo;
 }
 
-
-
 int Modulo::compare(const int rhs){
     int i=0; 
-    if (rhs==_value) i+=1; //if _value==_rhs i=1
-    if (rhs<_value) i+=3; //if rhs<=_value -> i=4
-    if (_value>rhs) i+=5; //if _value>=rhs -> i=6
+    if (rhs==(_value+_offset)) i+=1; //if _value==_rhs i=1
+    if (rhs<(_value+_offset)) i+=3; //if rhs<=_value -> i=4
+    if ((_value+_offset)>rhs) i+=5; //if _value>=rhs -> i=6
     //to prevent collision: chose odd number because their sum will be even
     return i; 
     
 }
 std::ostream& operator<<(std::ostream& ost, const Modulo& m){
 
-    ost<< m._value;
-
+    ost<< m._value+m._offset;
     return ost;
 }
 std::istream& operator>>(std::istream& ost, Modulo& m){
-    
-    m._value=(m._value-m._offset)%(m._modulo);
     ost >> m._value;
+    m._value=(m._value-m._offset)%(m._modulo);
     return ost; 
 }
 
