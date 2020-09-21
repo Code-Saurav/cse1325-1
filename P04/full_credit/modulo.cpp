5ae86a62f94038f4a9cd289f112aa73405f1bcde
hprabesh <humagain.prabesh@outlook.com>
2020-09-21 10:45:18 -0500

    Modulo and modulo2 -> main and main2.cpp file

--- P04/full_credit/modulo.cpp
+++ P04/full_credit/modulo.cpp
@@ -12,9 +12,10 @@ void Modulo::set_nmsd(Modulo *nmsd){
 
 Modulo& Modulo::operator+=(int rhs){
     this->_value=this->_value+rhs;
-    if (this->_value>=(this->_modulo+this->_offset)){
+    if (this->_value>=(this->_modulo+this->_offset)){ // compare with offset value and modulo rather than updating the streaming out
         this->_value=(this->_value)%(this->_modulo);
         if (this->_nmsd!=NULL){
+            this->_nmsd->_value=(this->_nmsd->_value)%(this->_nmsd->_modulo); // just in case the seg fault occurs
             _nmsd->operator+=(1); //recrusive call to update it's next most significant pointer
         }
     }
@@ -23,11 +24,11 @@ Modulo& Modulo::operator+=(int rhs){
 Modulo Modulo::operator+(int rhs){
     Modulo modulo {*this};
     modulo._value=modulo._value+rhs;
-    if (modulo._value>=modulo._modulo){
+    if (modulo._value>=(modulo._modulo+modulo._offset)){
         modulo._value=(modulo._value)%modulo._modulo;
         if (modulo._nmsd!=NULL){
+            modulo._nmsd->_value=(modulo._nmsd->_value)%(modulo._nmsd->_modulo);
             modulo._nmsd->operator+=(1);
-            // modulo._nmsd->_value=(modulo._nmsd->_value)%(modulo._nmsd->_modulo);
         }
     }
     return modulo;
@@ -35,11 +36,10 @@ Modulo Modulo::operator+(int rhs){
 Modulo& Modulo::operator++(){ //preincrement
     this->_value=(this->_value+1);
 
-    if (this->_value>=this->_modulo){
+    if (this->_value>=(this->_modulo+this->_offset)){
         this->_value=(this->_value)%this->_modulo;
         if (this->_nmsd!=NULL){
             this->_nmsd->_value=(this->_nmsd->_value)%(this->_nmsd->_modulo);
-            
             _nmsd->operator+=(1);
         }
     }
