02e0524c0f2d3a1d40877004fa9f70317d567f80
student <humagain.prabesh@outlook.com>
2020-09-26 18:57:44 -0500

    Full Credit Section Done

--- P05/full_credit/main.cpp
+++ P05/full_credit/main.cpp
@@ -11,6 +11,24 @@ int main(){
     cd->connect(*abcd,PINTWO);
     abcd->input(PINONE,SIGNALZERO);
     abcd->input(PINTWO, SIGNALZERO);
-    std::cout<<std::endl<<abcd->output()<<std::endl;
+    std::cout<<"A B C D  Q"<<std::endl;
+    std::cout<<"= = = =  ="<<std::endl;
+    for (int i=0;i<2;++i){
+        for (int j=0;j<2;++j){
+            for (int k=0;k<2;++k){
+                for (int l=0;l<2;++l){
+                    ab->input(PINONE,i%2);
+                    ab->input(PINTWO,j%2);
+                    cd->input(PINONE,k%2);
+                    cd->input(PINTWO,l%2);
+                    std::cout<<std::endl<<ab->input(PINONE)<<" ";
+                    std::cout<<ab->input(PINTWO)<<" ";
+                    std::cout<<cd->input(PINONE)<<" ";
+                    std::cout<<cd->input(PINTWO)<<"  ";
+                    std::cout<<abcd->output()<<std::endl;
+                }
+            }
+        }
+    }
 
 }
