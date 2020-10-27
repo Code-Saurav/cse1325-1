46c5f2864f9dc1946c0d3ca297d0d2a300d5e889
Prabesh Humagain <humagain.prabesh@outlook.com>
2020-10-26 21:53:42 -0500

    Glib error memory debugged

--- P07/main.cpp
+++ P07/main.cpp
@@ -1,7 +1,7 @@
 #include "mainwin.h"
 
 int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "MANGA Manager");
+    auto app = Gtk::Application::create(argc, argv, "MANGA.Manager");
     Mainwin win;
     return app->run(win);
 }
