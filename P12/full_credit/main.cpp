aaa937340af3f220b2fe50278cd296606b649aea
Humagain <prabesh.humagain@uta.edu>
2020-12-07 16:55:26 +0000

    Init

---
+++ P12/full_credit/main.cpp
@@ -0,0 +1,43 @@
+#include "mandelbrot.h"
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int width{1000}, height{width}, icount{60}, nthreads{1};
+    std::string filename = "default.ppm";
+
+    if (argc == 5)
+    {
+        try
+        {
+            width = std::stoi(argv[1]);
+            height = std::stoi(argv[2]);
+            icount = std::stoi(argv[3]);
+            filename = argv[4];
+        }
+        catch (std::exception &err)
+        {
+            std::cout << "\nInvalid argument" << std::endl;
+            std::cout << err.what() << std::endl;
+            return -2;
+        }
+    }
+
+    if (argc != 5 || argc != 1)
+        return -1;
+    std::cout << "Width: " << width << " Height: " << height << " icount: " << icount << std::endl;
+    Mandelbrot mandel(width, height, icount, nthreads);
+    try
+    {
+        std::ofstream ppm_file{filename};
+        ppm_file << mandel;
+    }
+    catch (std::exception &erno)
+    {
+        return -1;
+    }
+    int a;
+    std::cin >> a;
+    return 0;
+}
--- P12/full_credit/mandelbrot.h
+++ P12/full_credit/mandelbrot.h
@@ -2,24 +2,26 @@
 #define __MANDELBROT_H
 
 #include <complex>
+#include <iostream>
 
-class Mandelbrot {
-  public:
-    Mandelbrot(int width = 500, int height = 500, int icount = 60, int nthreads = 1);
-    ~Mandelbrot();
-    Mandelbrot(const Mandelbrot&) = delete;            // per Rule of 3
-    Mandelbrot& operator=(const Mandelbrot&) = delete; // per Rule of 3
-    friend std::ostream& operator<<(std::ostream& ost, const Mandelbrot& mandelbrot);
+class Mandelbrot
+{
+public:
+  Mandelbrot(int width = 500, int height = 500, int icount = 60, int nthreads = 1);
+  ~Mandelbrot();
+  Mandelbrot(const Mandelbrot &) = delete;            // per Rule of 3
+  Mandelbrot &operator=(const Mandelbrot &) = delete; // per Rule of 3
+  friend std::ostream &operator<<(std::ostream &ost, const Mandelbrot &mandelbrot);
 
-  protected:
-    void calculate_point (int x, int y);
-    void calculate_rows (int y1, int y2);
-    
-  private:
-    int _width;    // image x size (adjust for runtime)
-    int _height;   // image y size (adjust for runtime)
-    int _icount;   // iteration count
-    int* _values;  // array containing color values at y*width+x
+protected:
+  void calculate_point(int x, int y);
+  void calculate_rows(int y1, int y2);
+
+private:
+  int _width;   // image x size (adjust for runtime)
+  int _height;  // image y size (adjust for runtime)
+  int _icount;  // iteration count
+  int *_values; // array containing color values at y*width+x
 };
 
 #endif
